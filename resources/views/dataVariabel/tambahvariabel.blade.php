0ef6fdaa144f75ca6ee1e040954b41ec00a44457
akgira7 <you@example.com>
2025-04-27 21:48:50 +0700

    update

--- resources/views/dataVariabel/tambahvariabel.blade.php
+++ resources/views/dataVariabel/tambahvariabel.blade.php
@@ -7,7 +7,7 @@
     <div class="container-fluid">
         <div class="row mb-2">
         <div class="col-sm-6">
-            <h1 class="m-0">Tambah Data Variabel</h1>
+            <h1 class="m-0">Tambah Data Variabell</h1>
         </div><!-- /.col -->
         {{-- <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
